5b4cf90ba321851d6adaa7c616f9237dbb7ba9a1
David Hagege <david.hagege@gmail.com>
2015-02-06 12:07:21 +0900

    Fix imports and factorize.

--- lib/genatine.rb
+++ lib/genatine.rb
@@ -4,34 +4,36 @@ require 'active_support/inflector'
 module Genatine
   class Genatine
     def self.generate(model_name, attrs=[])
+    capitalized = model_name.capitalize
+
     sample =<<-EOS
     package models
 
     import slick.dao.{SlickDaoProfile, Entity}                                                                                                                                                                                                                                                                 
     import play.api.libs.json.Json
 
-    case class #{model_name.capitalize}(
+    case class #{capitalized}(
       #{attrs.map{|x| case_class(x) + ","}.join("\n")}
       id: Option[Long] = None
-    ) extends Entity[#{model_name.capitalize}] {
-      def withId(id: Long): #{model_name.capitalize} = copy(id = Some(id))
+    ) extends Entity[#{capitalized}] {
+      def withId(id: Long): #{capitalized} = copy(id = Some(id))
     }
 
-    object #{model_name.capitalize}  {
-      implicit val format#{model_name.capitalize} = Json.format[#{model_name.capitalize}]
+    object #{capitalized}  {
+      implicit val format#{capitalized} = Json.format[#{capitalized}]
     }
 
-    trait #{model_name.capitalize}Component  { this: SlickDaoProfile =>
-      import #{model_name.downcase}.simple._
+    trait #{capitalized}Component  { this: SlickDaoProfile =>
+      import profile.simple._
 
-      class #{model_name.capitalize.pluralize}Table(tag: Tag) extends BaseTable[#{model_name.capitalize}](tag, "#{model_name.downcase.pluralize}") {
+      class #{capitalized.pluralize}Table(tag: Tag) extends BaseTable[#{capitalized}](tag, "#{model_name.downcase.pluralize}") {
         def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
         #{attrs.map{|x| column(x)}.join("\n")}
 
-        def * = (#{attrs.map{|x| x.split(':').first}.join(',')}, id.?) <> ((#{model_name.capitalize}.apply _).tupled, #{model_name.capitalize}.unapply _)
+        def * = (#{attrs.map{|x| x.split(':').first}.join(',')}, id.?) <> ((#{capitalized}.apply _).tupled, #{capitalized}.unapply _)
       }
 
-      implicit object #{model_name.capitalize.pluralize} extends BaseTableQuery[#{model_name.capitalize}, #{model_name.capitalize.pluralize}Table](new #{model_name.capitalize.pluralize}Table(_)) {}
+      implicit object #{capitalized.pluralize} extends BaseTableQuery[#{capitalized}, #{capitalized.pluralize}Table](new #{capitalized.pluralize}Table(_)) {}
     }
     EOS
     end
